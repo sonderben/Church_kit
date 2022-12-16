1d4a2ac103b556ebcb521bd161b44616e04eff0e
YOUTE01806251 <johneyoute@gMAIL.com>
2022-12-15 19:26:46 -0500

    Push
    genial

--- app/src/main/java/com/churchkit/churchkit/MainActivity.java
+++ app/src/main/java/com/churchkit/churchkit/MainActivity.java
@@ -1,14 +1,61 @@
 package com.churchkit.churchkit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toolbar;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    MaterialToolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        toolbar=findViewById(R.id.toolbar);
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        NavController navController = navHostFragment.getNavController();
+        //the above code gets a reference to the navigation controller fromthe navigation host
+        AppBarConfiguration.Builder builder = new AppBarConfiguration.Builder(navController.getGraph());
+        AppBarConfiguration appBarConfiguration = builder.build();
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+        appBarConfiguration = builder.setOpenableLayout(drawerLayout).build();
+//builder
+        builder.setOpenableLayout(drawerLayout);
+
+        builder.setOpenableLayout(drawerLayout);
+        NavigationView navView = findViewById(R.id.nav_view);
+        builder.setOpenableLayout(drawerLayout);
+        NavigationUI.setupWithNavController(navView, navController);
+        NavigationUI.setupWithNavController(toolbar, navController, appBarConfiguration);
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        NavController navController =
+                Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.onNavDestinationSelected(item,
+                navController) || super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/churchkit/churchkit/ui/HistoryFragment.java
@@ -0,0 +1,21 @@
+package com.churchkit.churchkit.ui;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.churchkit.churchkit.R;
+
+public class HistoryFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_history, container, false);
+    }
+}
